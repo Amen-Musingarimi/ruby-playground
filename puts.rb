b667e78fd55f5a18c8f762d20bf2f74e29b6118d
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-17 23:10:47 +0200

    Add a variable

--- puts.rb
+++ puts.rb
@@ -1,3 +1,9 @@
 puts "Hello World 1"
-p "Hello World 2"
-print "Hello World 3"
+# p "Hello World 2"
+# print "Hello World 3"
+
+#------ Variable-----------
+greeting = "Hello World (from the variable)"
+puts greeting
+
+#------ Method-----------
