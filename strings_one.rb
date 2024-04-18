d93753d1a55a8335b43b314afa546bbbb6307074
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-18 08:11:21 +0200

    Demostrate to define a string with single and double quotes

---
+++ strings_one.rb
@@ -0,0 +1,5 @@
+# -------------String concatenation-----------
+sentence_name = "My name is Amen" # using double quotes
+sentence_surname = 'My lastname is Musingarimi' # using double quotes
+puts sentence_name
+puts sentence_surname
