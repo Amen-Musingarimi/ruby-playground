986488b0f11138755db603935a743db220514399
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-18 08:14:10 +0200

    Demostrate to define a string with single and double quotes

--- strings_one.rb
+++ strings_one.rb
@@ -3,3 +3,5 @@ sentence_name = "My name is Amen" # using double quotes
 sentence_surname = 'My lastname is Musingarimi' # using double quotes
 puts sentence_name
 puts sentence_surname
+
+#---------------String interpolation -----------------
