2c4fb554625e60ec8f6944db52c25d8a21716354
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-18 08:19:04 +0200

    Add notes on string concatenation

--- strings_one.rb
+++ strings_one.rb
@@ -1,4 +1,9 @@
 # -------------String concatenation-----------
+# String concatenation in Ruby refers to the process of combining two or more strings
+# into a single string. This is typically achieved using the + operator or the << operator. 
+# When you concatenate strings, you essentially append one string to the end of another, 
+# creating a longer string that contains the combined contents of the original strings.
+
 sentence_name = "My name is Amen" # using double quotes
 sentence_surname = 'My lastname is Musingarimi' # using double quotes
 puts sentence_name
